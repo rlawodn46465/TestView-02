bf1bfa5a7105683efb79d3bd3341f50f5fc4a4cd
dit08 <dit@imac-3.local>
2019-05-02 15:32:41 +0900

    동물과 나이

--- testview 02/ViewController.swift
+++ testview 02/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var animals = ["Cat", "Dog", "Cow", "Pig", "Horse", "Whale", "bird"]
+    var year = ["3", "5", "10", "2", "6", "3", "2"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // delegate 객체와 ViewController의 연결
+        myTableView.dataSource = self
     }
 
 
+    //
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    //cell을 생성
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //cell 생성, 재활용 풀 사용
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let row = indexPath.row
+        print("section = \(indexPath.section) row : \(indexPath.row)")
+        
+        cell.textLabel?.text = animals[row]
+        cell.detailTextLabel?.text = year[row]
+        
+        return cell
+    }
+    
 }
 
